13db81658d90ebe24da5054eda421093f347af02
Tyler <tyler@tylerrichard.dev>
2022-03-15 20:21:17 -0500

    first commit

---
+++ app/src/main/java/com/example/tylerricardc196/UI/MainActivity.java
@@ -0,0 +1,51 @@
+package com.example.tylerricardc196.UI;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.PopupMenu;
+
+import com.example.tylerricardc196.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void MainMenu(View view) {
+        PopupMenu mainPopUpMenu = new PopupMenu(MainActivity.this, view);
+        MenuInflater inflater = mainPopUpMenu.getMenuInflater();
+        inflater.inflate(R.menu.popup_menu, mainPopUpMenu.getMenu());
+        mainPopUpMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
+
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                String selection= (String) item.getTitle();
+                switch (selection) {
+                    case "Assignments":
+                        startActivity(new Intent(MainActivity.this, Assignments.class));
+                        return true;
+                    case "Courses":
+                        startActivity(new Intent(MainActivity.this, Courses.class));
+                        return true;
+                    case "Terms":
+                        startActivity(new Intent(MainActivity.this, Terms.class));
+                        return true;
+                    default:
+                        return false;
+                }
+            }
+        });
+        mainPopUpMenu.show();
+    }
+
+
+}
